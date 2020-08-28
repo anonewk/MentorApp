7a74d24bfb58a2c66acc09de22233f8ad0c941d5
AnoneWk <anonewk0@gmail.com>
2020-08-28 16:15:09 +0200

    Front Contrat et Relations

--- src/src/Controller/MentorController.php
+++ src/src/Controller/MentorController.php
@@ -50,4 +50,46 @@ class MentorController extends AbstractController
 
         return $this->render('mentor/profil.html.twig',['User' => $user]);
     }
+    /**
+     * Controller pour afficher le profil de mentor
+     *
+     * @Route("/mentor_mentore/relations", name="mentor_mentore_relations")
+     * @param EntityManagerInterface $manager
+     * @return Response
+     * @throws \Exception
+     */
+    // TODO Changer la route, remplir la fonction, remplir le back sur la page twig
+    public function relation(EntityManagerInterface $manager)
+    {
+
+        /*dd($user);*/
+        $datetime = new \DateTime();
+        $date = $datetime->format('Y-m-d');
+
+        return $this->render('mentor/relations/relations.html.twig');
+    }
+
+    /**
+     * Controller pour afficher le profil de mentor
+     *
+     * @Route("/mentor_mentore/relations/contrat", name="mentor_mentore_relations_contrat")
+     * @param EntityManagerInterface $manager
+     * @return Response
+     * @throws \Exception
+     */
+
+    // TODO Changer la route, remplir la fonction, remplir le back sur la page twig
+    public function contrat(EntityManagerInterface $manager){
+
+        /*dd($user);*/
+        $datetime = new \DateTime();
+        $date = $datetime->format('Y-m-d');
+
+        return $this->render('mentor/relations/contrat/contrat.html.twig');
+    }
+
+    public function contratObjectifs(EntityManagerInterface $manager)
+    {
+
+    }
 }
